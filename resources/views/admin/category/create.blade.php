12d99751ae8dd7007d001ce83887f226c5e1cf04
betulbedel <bedel69@hotmail.com>
2022-04-13 22:00:43 +0300

    admin category created

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.adminbase')
+
+@section('title','Add Category')
+
+@section('content')
+
+
+    <!--PAGE CONTENT -->
+    <div id="content">
+
+        <div class="inner" style="min-height: 700px;">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1> Add Category </h1>
+                </div>
+            </div>
+
+            <!--PAGE CONTENT -->
+            <div id="content">
+
+                <div class="inner" style="...">
+                    <div class="row">
+                        <div class="col-lg-20">
+                            <h3 class="page-header">Category Elements</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-20">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    Basic Elements
+                                </div>
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <form role="form">
+                                                <div class="form-group">
+                                                    <label>e-mail</label>
+                                                    <input class="form-control">
+                                                    <p class="help-block">Example block-level help text here.</p>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Password</label>
+                                                    <input class="form-control" placeholder="Enter text">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Static Control</label>
+                                                    <p class="form-control-static">email@example.com</p>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>File input</label>
+                                                    <input type="file" />
+                                                </div>
+                                                <button type="submit" class="btn btn-default">Submit Button</button>
+
+
+
+
+
+                </div>
+
+                <!--END BLOCK SECTION -->
+            <hr />
+
+
+
+
+
+
+
+
+    </div>
+    <!-- END RIGHT STRIP  SECTION -->
+    </div>
+
+    <!--END MAIN WRAPPER -->
+
+
+
+
+@endsection
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.adminbase')
+
+@section('title','Category List')
+
+@section('content')
+
+
+    <!--PAGE CONTENT -->
+    <div id="content">
+
+        <div class="inner" style="min-height: 700px;">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1> Category List </h1>
+                </div>
+            </div>
+            <hr />
+
+            <!--PAGE CONTENT -->
+            <div id="content">
+
+                <div class="inner" style="min-height:1200px;">
+                    <div class="row">
+                        <div class="col-lg-12">
+
+
+                        </div>
+                    </div>
+
+
+
+
+
+
+                </div>
+
+                <!--END BLOCK SECTION -->
+            <hr />
+
+
+
+
+
+
+
+
+    </div>
+    <!-- END RIGHT STRIP  SECTION -->
+    </div>
+
+    <!--END MAIN WRAPPER -->
+
+
+
+
+@endsection
