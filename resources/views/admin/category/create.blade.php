18a9d2d3dcff6deb40238e0895b23e5971688adb
betulbedel <bedel69@hotmail.com>
2022-04-30 16:32:47 +0300

    menu tree, sub category

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -33,6 +33,15 @@
                                         <div class="col-lg-6">
                                             <form role="form" action="{{route('admin.category.store')}}" method="post" enctype="multipart/form-data">
                                                 @csrf
+                                                <div class = "form-group">
+                                                    <label  >Parent Category </label>
+                                                    <select class="form-control select2" name="parent_id" style="...">
+                                                        <option value="0" selected="selected">Main Category</option>
+                                                        @foreach($data as $rs)
+                                                            <option value="{{$rs->id}}"> {{\App\Http\Controllers\AdminPanel\CategoryController::getParentsTree($rs, $rs->title)}}</option>
+                                                        @endforeach
+                                                    </select>
+
                                                 <div class="form-group">
                                                     <label>Title</label>
                                                     <input  type="text" class="form-control" name="title" placeholder="Title">
--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -33,7 +33,22 @@
                                         <div class="col-lg-6">
                                             <form role="form" action="{{route('admin.category.update',['id'=>$data->id ])}}" method="post" enctype="multipart/form-data">
                                                 @csrf
-                                                <div class="form-group">
+                                                @csrf
+                                                <div class = "card-body">
+                                                    <div class = "form-group">
+                                                    <label  >Parent Category </label>
+
+                                                    <select class="form-control select2" name="parent_id" style="...">
+                                                        <option value="0" selected="selected">Main Category</option>
+                                                        @foreach($datalist as $rs)
+                                                            <option value="{{$rs->id}}" @if($rs->id == $data->parent_id) selected ="selected" @endif
+                                                            > {{\App\Http\Controllers\AdminPanel\CategoryController::getParentsTree($rs, $rs->title)}}</option>
+                                                        @endforeach
+                                                    </select>
+                                                    </div>
+
+
+                                                    <div class="form-group">
                                                     <label>Title</label>
                                                     <input  type="text" class="form-control" name="title" value="{{$data->title}}" >
                                                 </div>
