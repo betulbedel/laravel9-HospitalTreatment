962c0350a492013713258b5c8b857e9e1ee949c1
betulbedel <bedel69@hotmail.com>
2022-05-28 17:25:53 +0300

    Contact Form2 Messages Home & Admin

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -24,25 +24,34 @@
                         <div class="page-section">
                             <div class="container">
                                 <h1 class="text-center wow fadeInUp">Get in Touch</h1>
+                              @include ('home.messages')
 
-                                <form class="contact-form mt-5">
+                                <form class="contact-form mt-5" action="{{route("storemessage")}}" method="post">
+                                    @csrf
                                     <div class="row mb-3">
                                         <div class="col-sm-6 py-2 wow fadeInLeft">
                                             <label for="fullName">Name</label>
-                                            <input type="text" id="fullName" class="form-control" placeholder="Full name..">
+                                            <input type="text" id="fullName" name="name" class="form-control" placeholder="Full name">
                                         </div>
                                         <div class="col-sm-6 py-2 wow fadeInRight">
                                             <label for="emailAddress">Email</label>
-                                            <input type="text" id="emailAddress" class="form-control" placeholder="Email address..">
+                                            <input type="text" id="emailAddress" name="email" class="form-control" placeholder="Email address">
+                                        </div>
+                                        <div class="col-12 py-2 wow fadeInUp">
+                                            <label for="phone">Phone Number</label>
+                                            <input type="tel" id="phone"  name="phone" class="form-control" placeholder="Phone Number">
                                         </div>
                                         <div class="col-12 py-2 wow fadeInUp">
                                             <label for="subject">Subject</label>
-                                            <input type="text" id="subject" class="form-control" placeholder="Enter subject..">
+                                            <input type="text" id="subject" name="subject" class="form-control" placeholder="Subject">
                                         </div>
+
                                         <div class="col-12 py-2 wow fadeInUp">
                                             <label for="message">Message</label>
-                                            <textarea id="message" class="form-control" rows="8" placeholder="Enter Message.."></textarea>
+                                            <textarea id="message" name="message" class="form-control" rows="8" placeholder="Enter Message.."></textarea>
                                         </div>
+
+
                                     </div>
                                     <button type="submit" class="btn btn-primary wow zoomIn">Send Message</button>
                                 </form>
